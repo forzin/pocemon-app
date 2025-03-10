d031e20ec60f948db981fc902461abdfa97a061d
Ihor Barban <barbanihor@gmail.com>
2025-03-10 02:05:15 +0100

    before StoryBook implemented

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,11 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-
-const config: StorybookConfig = {
-  framework: '@storybook/react-vite', // Для React з Vite
-  stories: ['../src/**/*.stories.tsx'], // Шлях до історій
-  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
-  docs: {
-    autodocs: 'tag',
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
   },
-  staticDirs: ['../public'],
 };
-
-export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@storybook/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './.storybook/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
